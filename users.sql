4cd6e3ede5073c733d7189d215da48bc757ea167
Khoahuynh1903 <khoahuynh1903@gmail.com>
2022-12-08 09:20:14 +0700

    update database table
    update table:
    - game
    - users

--- users.sql
+++ users.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2022 at 04:43 AM
+-- Generation Time: Dec 08, 2022 at 03:18 AM
 -- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- PHP Version: 8.0.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,19 +33,20 @@ CREATE TABLE `users` (
   `pass` text NOT NULL,
   `money` text NOT NULL,
   `fv_game` text NOT NULL,
-  `dl_game` text NOT NULL
+  `dl_game` text NOT NULL,
+  `type` int(5) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user`, `pass`, `money`, `fv_game`, `dl_game`) VALUES
-(13, 'adc05102002@gmail.com', '123456', '1.000.000', 'action_game 1/naction_game 3/npuzzle_game 2/nmoba_game 1/naction_game 2/n', 'action_game 2/nmoba_game 3/nmoba_game 2/npuzzle_game 7/naction_game 3/naction_game 1/n'),
-(14, 'abc@gmail.com', '1234567', '2.000.000', '', ''),
-(16, 'azx@gmail.com', '123', '2.500.000', '', ''),
-(17, 'abcz@gmail.com', '123456', '1.500.000', '', ''),
-(21, 'abcd@gmail.com', '300498', '', 'action_game 9/n', 'action_game 2/n');
+INSERT INTO `users` (`id`, `user`, `pass`, `money`, `fv_game`, `dl_game`, `type`) VALUES
+(13, 'adc05102002@gmail.com', '123456', '1.000.000', 'action_game 1/naction_game 3/npuzzle_game 2/nmoba_game 1/naction_game 2/n', 'action_game 2/nmoba_game 3/nmoba_game 2/npuzzle_game 7/naction_game 3/naction_game 1/n', 0),
+(14, 'abc@gmail.com', '1234567', '2.000.000', '', '', 1),
+(16, 'azx@gmail.com', '123', '2.500.000', '', '', 0),
+(17, 'abcz@gmail.com', '123456', '1.500.000', '', '', 0),
+(21, 'abcd@gmail.com', '300498', '', 'action_game 9/n', 'action_game 2/n', 0);
 
 --
 -- Indexes for dumped tables
